2f4834c33cf69c884279c26cb3bce79cb494a11e
박광주 <kjpar0317@innogrid.com>
2024-11-18 11:00:53 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtLayout v-slot="{ pageProps }">
+      <NuxtPage v-bind="pageProps" />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts">
+import "@/assets/styles/tailwind.css";
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>
+    <p>This page will be displayed at the /about route.</p>
+  </section>
+  <InputText placeholder="Overriden" class="!bg-slate-600" />
+</template>
