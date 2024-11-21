23404f147e6b76d51b63d1c3e35f398194049c63
박광주 <kjpar0317@innogrid.com>
2024-11-21 10:17:31 +0900

    minor patch

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,10 +1,10 @@
+<script setup lang="ts">
+import ItemList from "~/components/features/dashboard/ItemList.vue";
+</script>
+
 <template>
   <div>
     <h1>Dashboard</h1>
   </div>
   <ItemList />
 </template>
-
-<script setup lang="ts">
-import ItemList from "~/components/features/dashboard/ItemList.vue";
-</script>
